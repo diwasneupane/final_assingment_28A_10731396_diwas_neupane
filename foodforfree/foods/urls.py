7556d898ace5b436600ed7c7ee3f19f8c91bb310
diwas neupane <diwas.neupane55@gmail.com>
2021-09-30 11:21:11 +0545

    deleting function in category

--- foodforfree/foods/urls.py
+++ foodforfree/foods/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('homepage', views.homepage),
     path('category_form', views.category_form),
     path('get_category', views.get_category),
     path('delete_category/<int:category_id>', views.delete_category),
