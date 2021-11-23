edcd386032ccc5b49d636efb9ba5575be3d2ca02
diwas_neupane <diwas.neupane55@gmail.com>
2021-11-23 17:07:11 +0545

    Update urls.py

--- foodforfree/foodforfree/urls.py
+++ foodforfree/foodforfree/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('foods/', include('foods.urls')),
     path('admins/', include('admins.urls')),
-    path('', include('accounts.urls'))
-
+    path('', include('accounts.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+
